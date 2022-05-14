e5deed570ef3706c4e4760c9bf0f0730e880557b
Razeen <razeenb@bbd.co.za>
2022-05-14 22:58:57 +0200

    added /user routes for creating a profile, updating intro & uploading avatar

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,10 +1,8 @@
 var express = require('express');
-const { resourceLimits } = require('worker_threads');
-
 var validateRequest = require('../middleware/requestAuthenticator');
 var db_client = require('../database.js');
 
-
+const fileUploader = require('../middleware/fileUpload');
 
 var router = express.Router();
 router.use(validateRequest);
@@ -21,7 +19,7 @@ const {
 
 
 const validateRequestBody = require('../utils/requestBodyValidator');
-const { body } = require('express-validator');
+const { body, check } = require('express-validator');
 /************************************************
   FAVOURITE ROUTES & CRUD OPS
 ************************************************/
@@ -132,7 +130,152 @@ const removeFavourite = async (favourite) => {
 
 
 
+/************************************************
+  USER ROUTES & CRUD OPS
+************************************************/
 
+
+// create profile
+router.post('/user/profile',validateRequestBody([body('username').isString().isLength({min:9,max:99}),body('intro').isString().isLength({max:255}), body('avatarUrl').isURL().isLength({max : 50})]), function(req,res,next) {
+    
+  profile = {
+    username: req.body.username,
+    intro: req.body.intro,
+    avatarUrl: req.body.avatarUrl
+  }
+
+  createProfile(profile)
+  .then(results => {
+    sendHttpSuccess(req,res,null);
+  })
+  .catch(err => {
+    sendHttpError(req,res,err);
+  });
+})
+
+
+const createProfile = async (profile) => {
+return new Promise((resolve,reject) => {
+    let query = "INSERT INTO profile SET username = :username, intro = :intro, avatarUrl = :avatarUrl;"
+    db_client.query(query
+    ,profile, 
+    function (err,resultSet) {
+      if (err) {
+          err.response = "Failed to create user profile"
+          reject(err);
+      } else {
+          resolve(resultSet);
+      }
+    });
+  
+});
+}
+
+
+// get profile
+router.get('/user/profile', function(req,res,next) {
+  username = {
+    username : req.user.username
+  }
+  getProfile(username)
+  .then(resultSet => {
+    return sendHttpSuccess(req,res,resultSet);
+  })
+  .catch(err => {
+    sendHttpError(req,res,err);
+  });
+
+})
+
+
+const getProfile = async (username) => {
+return new Promise((resolve,reject) => {
+
+  let query = "SELECT  p.username, p.intro, p.avatarUrl from profile p where username = :username"
+  db_client.query(query
+    ,username, 
+    function (err,resultSet) {
+      if (err) {
+          err.response = "Failed to get user profile"
+          reject(err);
+      } else {
+          resolve(resultSet);
+      }
+    });
+
+});
+}
+
+// update intro
+router.put('/user/intro', validateRequestBody([body('intro').isString().isLength({max:255})]), function(req,res,next) {
+intro = {
+  intro: req.body.intro,
+  username: req.user.username
+}
+updateIntro(intro)
+.then(resultSet => {
+  return sendHttpSuccess(req,res,null);
+})
+.catch(err => {
+  return sendHttpError(req,res,err);
+});
+})
+
+
+
+const updateIntro = async (intro) => {
+return new Promise((resolve,reject) => {
+    let query = "UPDATE profile SET intro = :intro WHERE username = :username"
+    db_client.query(query
+      ,intro, 
+      function (err,resultSet) {
+        if (err) {
+            err.response = "Failed to update intro"
+            reject(err);
+        } else {
+            resolve(resultSet);
+        }
+      });
+});
+}
+
+
+
+
+// upload new avatar
+router.put('/user/avatar',fileUploader.fileUploader, function(req, res ,next)  {
+  if (!req.file) {
+    return sendHttpError(req,res,{response:"Failed to upload avatar"});
+  } else {
+    url = {
+        avatarUrl: req.file.location,
+        username: req.user.username
+    }
+    updateAvatarUrl(url)
+    .then(resultSet => {
+        return sendHttpSuccess(req,res,null);
+    }).catch(err => {
+      return sendHttpError(req,res,err);
+    })
+  }
+});
+
+
+const updateAvatarUrl = async (url) => {
+  return new Promise((resolve,reject) => {
+      let query = "UPDATE profile SET avatarUrl = :avatarUrl WHERE username = :username;";
+     let x = db_client.query(query
+        ,url, 
+        function (err,resultSet) {
+          if (err) {
+              err.response = "Failed to upload avatar"
+              reject(err);
+          } else {
+              resolve(resultSet);
+          }
+        });  
+      });
+}
 
 
 
