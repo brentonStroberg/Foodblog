b2b6f71fed4a2754691029897e726a69e325fae1
brentonStroberg <brentons@bbd.co.za>
2022-05-16 22:38:52 +0200

    navbar

--- Client/js/navbar.js
+++ Client/js/navbar.js
@@ -1,10 +1,17 @@
-function toggleNavbar(){
+function onload(){
+    setNavbar();
+}
 
+function setNavbar(){
+    document.getElementById("sidebar").style.display = "none";
+}
+
+function toggleNavbar(){
     let element = document.getElementById("sidebar");
     console.log(element);
     console.log(element.style.display)
     if(element.style.display === "" || element.style.display==="none"){
-        document.getElementById("sidebar").style.display = "flex";
+        document.getElementById("sidebar").style.display = "block";
     }else{
         document.getElementById("sidebar").style.display = "none";
     }
