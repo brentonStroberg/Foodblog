3099388d4405c079f1916fb62ec14f844a1ac5a0
tarshenNaidooBBD <99730660+tarshenNaidooBBD@users.noreply.github.com>
2022-05-19 14:07:22 +0200

    Removed

--- Client/js/register.js
+++
@@ -1,9 +0,0 @@
-function switch_to_OTP() {
-    console.log('switching');
-    let registerfields_list = document.getElementById('register_form');
-    registerfields_list.style.display = 'none'
-    //for (let i=0; i < registerfields_list.length; i++) {
-    //   registerfields_list[i].style.display = 'none';
-    //}
-    document.getElementById('OTP_section').style.display = 'block';
-}
