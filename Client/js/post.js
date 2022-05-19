bfc4d7d7cb6360ea7a0b854b88bccaec60fcbb65
Abie Knepscheld <abrahamk@bbd.co.za>
2022-05-19 13:40:09 +0200

    Merged Develop and added new changes

---
+++ Client/js/post.js
@@ -0,0 +1,10 @@
+let input = document.querySelector('input[type="file"]')
+
+let data = new FormData()
+data.append('file', input.files[0])
+data.append('user', 'hubot')
+
+fetch('/avatars', {
+  method: 'POST',
+  body: data
+})
