e937432c457f7ea6d319f840a0a1ceca7866a9fc
Razeen <razeenb@bbd.co.za>
2022-05-19 15:10:18 +0200

    Added dynamic cors

--- Server/app.js
+++ Server/app.js
@@ -23,11 +23,28 @@ app.use(cookieParser());
 
 
 
-
+var allowedOrigins = ['http://localhost:5500',
+                      'http:127.0.0.1:5500'];
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
-    credentials: true
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
+
+
+// app.use(cors({
+//     origin: 'http://127.0.0.1:5500',
+//     credentials: true
+// }));
 
 
 app.use('/api', authenticatedRoute);
