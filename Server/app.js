fcd390489c93aa0711c19113a09faf5145e905de
Razeen <razeenb@bbd.co.za>
2022-05-19 18:39:22 +0200

    added custom cors

--- Server/app.js
+++ Server/app.js
@@ -19,18 +19,35 @@ var allowedOrigins = ['http://localhost:5500',
                       'http://127.0.0.1:5500','undefined'];
 
 
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS with origin ' + origin))
-    }
-  },
-  credentials: true
-}
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS with origin ' + origin))
+//     }
+//   },
+//   credentials: true
+// }
 
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+ 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+ 
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+ 
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+    // Pass to next layer of middleware
+    next();
+ });
 
 
 // app.use(logger('dev'));
