8bba026f78633afdb92f1b263be4bf3c7410ab4e
Tshiamo <tshiamo.mahloko@vumatel.co.za>
2022-05-19 19:23:42 +0200

    Fetch for more post sections

--- Client/js/home.js
+++ Client/js/home.js
@@ -1,8 +1,10 @@
 loadFunction = () => {
+  fetchFavourites()
   fetchRecents()
+  fetchExplore()
 }
 
-fetchRecents = () => {
+fetchFavourites = () => {
   let favourites_endpoint = `http://${hosts[currentHost]}${endpoints.getFavourites}`
 
   let { endpoint, request } = new ApiCall(favourites_endpoint, 'GET')
@@ -17,12 +19,66 @@ fetchRecents = () => {
 
   Promise.allSettled([callAPI(endpoint, request)]).then((results) => {
     console.log(results[0].value)
-    populateCarousel(results[0].value)
+    populateFavourites(results[0].value)
   })
 }
 
-populateCarousel = (posts) => {
-  let post = document.getElementById('posts-container')
+fetchRecents = () => {
+  let recentEndpoint = `http://${hosts[currentHost]}${endpoints.getRecentPosts}`
+  let { endpoint, request } = new ApiCall(recentEndpoint, 'GET')
+    .withCredentials()
+    .withHeader(
+      'Authorization',
+      `Bearer eyJraWQiOiJ5VzE5UDNadExTaDJLQXdqbVUwemk0OXlFTmJTamhoK3VmMXlQekF6WHB3PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiM2IxMmEwYy1kMjU3LTRiNDctOTBmZC1jMzY1M2U1MTExZTgiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV8xWkhnd3M1aHMiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiIzcjJwc2VuNTJkZ2tpbmJqdXZub3MzanZvZSIsImV2ZW50X2lkIjoiMDQ0ZTM4NTgtNjg3NC00YjljLWFiM2EtYzdiNzU0MWU3N2RmIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiBwaG9uZSBvcGVuaWQgZW1haWwiLCJhdXRoX3RpbWUiOjE2NTI5NjAzNjEsImV4cCI6MTY1MzA0Njc2MSwiaWF0IjoxNjUyOTYwMzYxLCJqdGkiOiIxMzQ0MmU3MS1iMzE0LTRhNDUtODIxMy00MzllMmNiYTBhZmYiLCJ1c2VybmFtZSI6IlJhemVlbiJ9.YrdPUxvfVOBUsMFJWMDUUpOtkHaCieLtBSjJx_IlvfqpzZRzB-6veUUgpXZoed4h0pVJTZoqcrUcdqTpUAAUGv3QDRhWtkB88hv36h_a6h2UFI8Xj7yy_2MDSboaCviAIufAGTpRNnZchnmpy2_ng0FoDwfayi-qzH55n7D4MI1apJgywr7Jf1bSxcys-BelUhFi548YYRskVtFD7qethdbE-WHDtuzQiu9PM4ZW8WI5pdbSIRg6gkyS49ZARov1n0Pxc6ZB8Njbx7GUmxKfm71AdLAqUut5daA430bKdNILduo79iYazFPCd4hq4eCgsQowKgTVGrYF4PdbSPGXHg`
+    )
+    .withHeader('Content-Type', 'application/json')
+    .build()
+  Promise.allSettled([callAPI(endpoint, request)]).then((results) => {
+    console.log(results[0].value)
+    populateRecents(results[0].value)
+  })
+}
+
+fetchExplore = () => {
+  let recentEndpoint = `http://${hosts[currentHost]}${endpoints.getRecentPosts}`
+  let { endpoint, request } = new ApiCall(recentEndpoint, 'GET')
+    .withCredentials()
+    .withHeader(
+      'Authorization',
+      `Bearer eyJraWQiOiJ5VzE5UDNadExTaDJLQXdqbVUwemk0OXlFTmJTamhoK3VmMXlQekF6WHB3PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiM2IxMmEwYy1kMjU3LTRiNDctOTBmZC1jMzY1M2U1MTExZTgiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV8xWkhnd3M1aHMiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiIzcjJwc2VuNTJkZ2tpbmJqdXZub3MzanZvZSIsImV2ZW50X2lkIjoiMDQ0ZTM4NTgtNjg3NC00YjljLWFiM2EtYzdiNzU0MWU3N2RmIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiBwaG9uZSBvcGVuaWQgZW1haWwiLCJhdXRoX3RpbWUiOjE2NTI5NjAzNjEsImV4cCI6MTY1MzA0Njc2MSwiaWF0IjoxNjUyOTYwMzYxLCJqdGkiOiIxMzQ0MmU3MS1iMzE0LTRhNDUtODIxMy00MzllMmNiYTBhZmYiLCJ1c2VybmFtZSI6IlJhemVlbiJ9.YrdPUxvfVOBUsMFJWMDUUpOtkHaCieLtBSjJx_IlvfqpzZRzB-6veUUgpXZoed4h0pVJTZoqcrUcdqTpUAAUGv3QDRhWtkB88hv36h_a6h2UFI8Xj7yy_2MDSboaCviAIufAGTpRNnZchnmpy2_ng0FoDwfayi-qzH55n7D4MI1apJgywr7Jf1bSxcys-BelUhFi548YYRskVtFD7qethdbE-WHDtuzQiu9PM4ZW8WI5pdbSIRg6gkyS49ZARov1n0Pxc6ZB8Njbx7GUmxKfm71AdLAqUut5daA430bKdNILduo79iYazFPCd4hq4eCgsQowKgTVGrYF4PdbSPGXHg`
+    )
+    .withHeader('Content-Type', 'application/json')
+    .build()
+  Promise.allSettled([callAPI(endpoint, request)]).then((results) => {
+    console.log(results[0].value)
+    populateRecents(results[0].value)
+  })
+}
+
+populateFavourites = (posts) => {
+  let post = document.getElementById('posts-container-favs')
+
+  posts.forEach((x) => {
+    let item = document.createElement('section')
+    item.classList.add('post-card')
+    item.setAttribute('id', 'post-card')
+
+    let y = `<figure class="post-image">
+        <img src="${x.banner}" class="post-thumb" id="dish-img">
+    </figure>
+    <article class="post-info">
+        <h2 class="post-name">${x.title}</h2>
+        <p class="post-short-description">${x.content}</p>
+    </article>`
+
+    item.innerHTML = y
+    post.appendChild(item)
+    document.getElementById('post-card').addEventListener('click', onItemClick)
+  })
+}
+
+populateRecents = (posts) => {
+  let post = document.getElementById('posts-container-recents')
 
   posts.forEach((x) => {
     let item = document.createElement('section')
@@ -44,7 +100,6 @@ populateCarousel = (posts) => {
 }
 
 onItemClick = () => {
-  //   setCookie('testing', 'a bunch of info', 7)
   console.log('clicked')
 }
 
